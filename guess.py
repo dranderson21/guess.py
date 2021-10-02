617ad0e198c3616e485c2ce178b94ba8075f94ab
David Anderson <dranderson21@gmail.com>
2021-10-02 02:10:38 -0400

    Added counter for each attempt at guessing

--- guess.py
+++ guess.py
@@ -2,19 +2,23 @@ def main():
 
     import random
 
-    rnjesus = (random.randrange(1, 100, 1))
+    rnjesus = (random.randrange(1, 1000, 1))
 
     def correct_answer():
         if int(guess) == (rnjesus):
             print("Correct! " + str(rnjesus) + " is the number!")
+            print("It took you " + str(counter) + " attempts.")
 
-    guess = int(input("Guess a number 1-100: "))
+    guess = int(input("Guess a number 1-1000: "))
+    counter = 1
 
     while int(guess) != (rnjesus):
         if int(guess) > (rnjesus):
             guess = int(input("Lower: "))
+            counter = counter + 1
         elif int(guess) < (rnjesus):
             guess = int(input("Higher: "))
+            counter = counter + 1
 
     correct_answer()
     
