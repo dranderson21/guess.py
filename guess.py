9a09873ace72c238fce141227465dd77b917a24b
David Anderson <dranderson21@gmail.com>
2021-10-16 05:20:00 -0400

    General cleanup, added timer, added dialog box

--- guess.py
+++ guess.py
@@ -1,33 +1,22 @@
-def main():
+import time
+import random
+import ctypes
 
-    import random
+rnjesus = (random.randrange(1, 1000, 1))
 
-    rnjesus = (random.randrange(1, 1000, 1))
+def correct_answer():
+    if int(guess) == (rnjesus):
+        ctypes.windll.user32.MessageBoxW(0, str(rnjesus) + " is correct, " + str(counter) + " attempts in " + str(time.perf_counter()) + " seconds" "", "David's Guessing Game", 1)
 
-    def correct_answer():
-        if int(guess) == (rnjesus):
-            print("Correct! " + str(rnjesus) + " is the number!")
-            print("It took you " + str(counter) + " attempts.")
+guess = int(input("Guess a number 1-1000: "))
+counter = 1
 
-    guess = int(input("Guess a number 1-1000: "))
-    counter = 1
+while int(guess) != (rnjesus):
+    if int(guess) > (rnjesus):
+        guess = int(input("Lower: "))
+        counter = counter + 1
+    elif int(guess) < (rnjesus):
+        guess = int(input("Higher: "))
+        counter = counter + 1
 
-    while int(guess) != (rnjesus):
-        if int(guess) > (rnjesus):
-            guess = int(input("Lower: "))
-            counter = counter + 1
-        elif int(guess) < (rnjesus):
-            guess = int(input("Higher: "))
-            counter = counter + 1
-
-    correct_answer()
-    
-    again = input("Play Again? (y/n): ")
-    if again == "y":
-        main()
-    elif again == "n":
-        print("Closing Application...")
-    else:
-        print("Invalid Entry. Closing Application...")
-    
-main()
+correct_answer()
